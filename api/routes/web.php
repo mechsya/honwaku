1f8a7f1e0b8e88641deeb20164be9d4e1b752edb
mechsya <alinia.meysa@gmail.com>
2025-04-22 22:39:13 +0700

    add: add storage link in router

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AnnouncementController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::view("", "home.index");
@@ -8,4 +9,8 @@ Route::view("", "home.index");
 Route::prefix("announcement")->group(function () {
     Route::get("", [AnnouncementController::class, "get"]);
     Route::get("{slug}", [AnnouncementController::class, "show"]);
+});
+
+Route::get("storage:link", function () {
+    Artisan::call("storage:link");
 });
