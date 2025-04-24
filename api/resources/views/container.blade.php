1f1b88f6ae50ebaf08797c4cdfb0b9d482739262
mechsya <alinia.meysa@gmail.com>
2025-04-24 19:20:59 +0700

    + : add new tailwind build

--- api/resources/views/container.blade.php
+++ api/resources/views/container.blade.php
@@ -24,7 +24,7 @@
         href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
     @vite('resources/css/app.css')
-    <link rel="stylesheet" href="{{ asset('build/assets/app-Cd4R2YnF.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/app-CUOG8wtT.css') }}">
     <link rel="manifest" href="{{ asset('build/manifest.json') }}" />
     @yield('head')
 </head>
